69ed3eb1cfc812c9168b6942586c52f9e4170884
chrysr <sdi1500205@di.uoa.gr>
2019-09-12 18:48:51 +0300

    working all good

--- server/routes/myaccount/index.js
+++ server/routes/myaccount/index.js
@@ -19,10 +19,7 @@ module.exports = (param) =>{
                     const db = client.db("allyouneed");
                     var _id=req.cookies._id;
                     console.log(_id);
-                    //_id=_id.toString();
-                    console.log(_id);
-                    //var id = require('mongodb').ObjectID(doc._id);
-                    db.collection('users').find({"_id.$oid":require('mongodb').ObjectID(_id.toString())}).toArray().then((docs)=>{
+                    db.collection('users').find({"_id":require('mongodb').ObjectID(_id.toString())}).toArray().then((docs)=>{
                         console.log(docs);
                         return res.render("myaccount",{
                             page: "My Account",
