49f730f59f38a2d1ee8080211d3ef17f1ae68431
charambos88 <sdi1500191@di.uoa.gr>
2019-09-15 16:08:01 +0300

    new auction

--- server/routes/createauction/index.js
+++ server/routes/createauction/index.js
@@ -39,43 +39,60 @@ module.exports = (param) =>{
                     //success: req.query.success,
                 });
             }
-        } 
+        }
         catch (err) {
             return err;
         }
-        
+
     });
-    
+
     router.post("/",async(req,res,next) =>{
+        console.log("came here");
         try {
             const db=req.app.locals.db;
-            var description,name,shortname,startingbid,increment;
-            description=((req.body.description?req.body.description:null)?req.body.description.trim():null);
+            var name,category,currently,buy_price,starting_bid,bid_num,bids;
+            var bid,bidder,time,amount,location,country,started,ends,seller,product_descr;
             name=((req.body.name?req.body.name:null)?req.body.name.trim():null);
-            //shortname=((req.body.shortname?req.body.shortname:null)?req.body.shortname.trim():null);
-            startingbid=((req.body.startingbid?req.body.startingbid:null)?req.body.startingbid.trim():null);
-            increment=((req.body.increment?req.body.increment:null)?req.body.increment.trim():null);
-
+            category=((req.body.category?req.body.category:null)?req.body.category.trim():null);
+            currently=((req.body.currently?req.body.currently:null)?req.body.currently.trim():null);
+            buy_price=((req.body.buyprice?req.body.buyprice:null)?req.body.buyprice.trim():null);
+            starting_bid=((req.body.startingbid?req.body.startingbid:null)?req.body.startingbid.trim():null);
+            bid_num=((req.body.bidnum?req.body.bidnum:null)?req.body.bidnum.trim():null);
+            bids=((req.body.bids?req.body.bids:null)?req.body.bids.trim():null);
+            bid=((req.body.bid?req.body.bid:null)?req.body.bid.trim():null);
+            bidder=((req.body.bidder?req.body.bidder:null)?req.body.bidder.trim():null);
+            time=((req.body.time?req.body.time:null)?req.body.time.trim():null);
+            amount=((req.body.amount?req.body.amount:null)?req.body.amount.trim():null);
+            location=((req.body.location?req.body.location:null)?req.body.location.trim():null);
+            country=((req.body.country?req.body.country:null)?req.body.country.trim():null);
+            started=((req.body.started?req.body.started:null)?req.body.started.trim():null);
+            ends=((req.body.ends?req.body.ends:null)?req.body.ends.trim():null);
+            seller=((req.body.seller?req.body.seller:null)?req.body.seller.trim():null);
+            product_descr=((req.body.product_descr?req.body.product_descr:null)?req.body.product_descr.trim():null);
+            console.log("here1");
             for(i=2, flag=0;flag==0;i++)
             {
+              console.log("here2");
+
                 shortname=name+makeid(i);
                 db.collection('products').find({shortname:shortname}).toArray().then((docs)=>{
                     if(docs.length==0)
                         flag=1;
                 });
             }
+            console.log("here3");
+
             entry={description:description,name:name,date:new Date(),shortname:shortname,price:startingbid}
             db.collection('products').insertOne(entry).then((docs)=>{
                 console.log("product inserted successfully");
                 res.redirect('/createauction?success=true');
             })
-
-        
-                
-        } 
+        }
         catch (err) {
             return next(err);
         }
+        console.log("here4");
+
     });
 
     return router;
